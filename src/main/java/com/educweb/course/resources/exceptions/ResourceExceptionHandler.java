8e87f8283a617d9cb3dba3100a46c86209dd8ed7
enzomaneira <enzo@maneira.com>
2024-02-26 12:47:59 -0300

    insert update and delete user

---
+++ src/main/java/com/educweb/course/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.educweb.course.resources.exceptions;
+
+public class ResourceExceptionHandler {
+}
--- src/main/java/com/educweb/course/service/UserService.java
+++ src/main/java/com/educweb/course/service/UserService.java
@@ -3,7 +3,11 @@ package com.educweb.course.service;
 import com.educweb.course.entities.User;
 import com.educweb.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+
+
 
 import java.util.List;
 import java.util.Optional;
@@ -20,6 +24,28 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public User insert(User obj) {
+        return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    public User update(Long id, User obj) {
+        User entity = repository.getReferenceById(id);
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
     }
 }
+
+
